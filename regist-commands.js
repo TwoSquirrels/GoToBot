7753b917d4083e9c73f234412e34b361ad866614
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2021-08-30 12:14:12 +0900

    タブとスペースが混在していたのでスペースに統一

--- regist-commands.js
+++ regist-commands.js
@@ -5,28 +5,28 @@ const { Routes } = require("discord-api-types/v9");
 require("dotenv").config();
 
 const commands = [
-	new SlashCommandBuilder()
-	  .setName("goto")
-	  .setDescription("Reply GoTo image")
-	  .addStringOption(option => option.setName("行き先").setDescription("GoToの下に付く文字列").setRequired(true))
-	  .addStringOption(option => option.setName("英語").setDescription("行き先の英語").setRequired(false)),
+  new SlashCommandBuilder()
+    .setName("goto")
+    .setDescription("Reply GoTo image")
+    .addStringOption(option => option.setName("行き先").setDescription("GoToの下に付く文字列").setRequired(true))
+    .addStringOption(option => option.setName("英語").setDescription("行き先の英語").setRequired(false)),
 ]
-	.map(command => command.toJSON());
+  .map(command => command.toJSON());
 
 const rest = new REST({ version: "9" }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
-	try {
-	  await rest.put(
-	    Routes.applicationGuildCommands(process.env.BOT_ID, process.env.TEST_GUILD),
-	    { body: commands },
-	  );
-		await rest.put(
-			Routes.applicationCommands(process.env.BOT_ID),
-			{ body: commands },
-		);
-		console.log("Successfully registered application commands.");
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.BOT_ID, process.env.TEST_GUILD),
+      { body: commands },
+    );
+    await rest.put(
+      Routes.applicationCommands(process.env.BOT_ID),
+      { body: commands },
+    );
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error(error);
+  }
 })();
