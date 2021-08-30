66bc854012f397238593303777ef1d5c753c3a41
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2021-08-30 13:33:36 +0900

    エラー対策

--- main.ts
+++ main.ts
@@ -14,32 +14,36 @@ bot.once("ready", () => {
 });
 
 bot.on("interactionCreate", async interaction => {
-  if (interaction.isCommand()) {
-    const command = interaction as discord.CommandInteraction;
-    switch (command.commandName) {
-    case "goto":
-      const dest = command.options.getString("行き先", true);
-      let english = command.options.getString("英語", false);
-      if (english) {
-        english = english.toUpperCase();
-        await command.reply("画像を生成中...");
-      } else {
-        await command.reply("行き先を翻訳中...");
-        try {
-          const translated = await fetch(process.env.TRANSLATE_API + `?text=${encodeURIComponent(dest)}&source=ja&target=en`).then(res => res.json());
-          if (translated.code !== 200) throw new Error(translated.text);
-          english = (translated.text as string).toUpperCase();
-        } catch (err) {
-          console.error(err);
-          await command.editReply("翻訳に失敗しました。");
-          break;
+  try {
+    if (interaction.isCommand()) {
+      const command = interaction as discord.CommandInteraction;
+      switch (command.commandName) {
+      case "goto":
+        const dest = command.options.getString("行き先", true);
+        let english = command.options.getString("英語", false);
+        if (english) {
+          english = english.toUpperCase();
+          await command.reply("画像を生成中...");
+        } else {
+          await command.reply("行き先を翻訳中...");
+          try {
+            const translated = await fetch(process.env.TRANSLATE_API + `?text=${encodeURIComponent(dest)}&source=ja&target=en`).then(res => res.json());
+            if (translated.code !== 200) throw new Error(translated.text);
+            english = (translated.text as string).toUpperCase();
+          } catch (err) {
+            console.error(err);
+            await command.editReply("翻訳に失敗しました。");
+            throw new Error("翻訳に失敗しました。");
+          }
+          await command.editReply("画像を生成中...");
         }
-        await command.editReply("画像を生成中...");
+        await wait(3);
+        await command.editReply(`GoTo ${dest} (\`Go To ${english}\`)`);
+        break;
       }
-      await wait(3);
-      await command.editReply(`GoTo ${dest} (\`Go To ${english}\`)`);
-      break;
     }
+  } catch (err) {
+    console.error(err);
   }
 });
 
